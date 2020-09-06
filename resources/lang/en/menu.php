c0136c730ef85dd2a009a8df2b0e40adf6157f86
Rizky Putra <rizky_putra86@ymail.com>
2020-09-06 15:57:46 +0700

    procfile

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,11 @@
+<?php
+return[
+    'header-menu' => 'Admin Panel',
+    'menu1' => 'Company',
+    'submenu1' => 'List Company',
+    'menu2' => 'Employee',
+    'submenu2' => 'List Employee',
+    'l-company' => 'List Employee',
+    't-company' => 'Add Employee',
+    'e-company' => 'Edit Employee',
+];
---
+++ resources/lang/id/menu.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'header-menu' => 'Panel Admin',
+    'menu1' => 'Perusahaan',
+    'submenu1' => 'Daftar Perusahaan',
+    'menu2' => 'Karyawan',
+    'submenu2' => 'List Karyawan',
+    'l-company' => 'List Karyawan',
+    't-company' => 'Add Karyawan',
+    'e-company' => 'Edit Karyawan',
+];
